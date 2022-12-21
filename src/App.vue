060f7b2774c64a8c5e40ec0f0f68757c5fae1d27
Cortney <thomas.cortney@gmail.com>
2022-12-21 18:11:37 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import FortuneCookie from "./components/FortuneCookie.vue";
+</script>
+
+<template>
+    <FortuneCookie />
+</template>
+
---
+++ src/components/FortuneCookie.vue
@@ -0,0 +1,28 @@
+<template>
+    <p>{{ fortune }}</p>
+    <button @click.prevent="handleCookieClick">Click me</button>
+</template>
+
+<script>
+
+export default {
+    name: "fortune-cookie",
+    components: {
+
+    },
+    data() {
+        return {
+            fortune: "test"
+        }
+    },
+    methods: {
+        handleCookieClick() {
+            console.log('cookie clicked')
+            // todo fetch data from API
+            this.fortune = "clicked"
+        }
+
+    }
+}
+
+</script>
